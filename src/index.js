177d556b3be97406e01f91741d44792884738f02
Keith Tachibana <Keith.Tachibana@gmail.com>
2020-03-29 02:18:00 -0700

    Updated API key

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyC1PBjdlV2COCnVy29OkYZsk6AVJb7KIrM';
+const API_KEY = 'AIzaSyBt85ksQED2FubwPmuZewWluMcQtbgDJD4';
 
 class App extends Component {
   constructor(props) {
